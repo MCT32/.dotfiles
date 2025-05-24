dde69f850d6af9682e9bb95e9c759e013ccf7cbc
Seth Henderson <32090502+MCT32@users.noreply.github.com>
2025-05-24 23:35:49 +1000

    qutebrowser more search engines

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -3,6 +3,14 @@ import dracula.draw
 # Load existing settings made via :set
 config.load_autoconfig()
 
+c.url.searchengines = {
+    'DEFAULT': 'https://duckduckgo.com/?q={}',
+    '!aw': 'https://wiki.archlinux.org/?search={}',
+    '!apkg': 'https://archwiki.org/packages/?sort=&q={}&maintainer=&flagged=',
+    '!gh': 'https://github.com/search?o=desc&q={}&s=stars',
+    '!yt': 'https://www.youtube.com/results?search_query={}',
+}
+
 dracula.draw.blood(c, {
     'spacing': {
         'vertical': 6,
