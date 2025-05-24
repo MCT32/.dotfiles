67fed664871184c21805275217e376dbf726febc
Seth Henderson <32090502+MCT32@users.noreply.github.com>
2025-05-24 22:11:59 +1000

    Fish config

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,5 +1,6 @@
 ln -sf $PWD/hypr $HOME/.config
 ln -sf $PWD/waybar $HOME/.config
 ln -sf $PWD/kitty $HOME/.config
+ln -sf $PWD/fish $HOME/.config
 ln -sf $PWD/.gitconfig $HOME
 ln -sf $PWD/.vimrc $HOME
