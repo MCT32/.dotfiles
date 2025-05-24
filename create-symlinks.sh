c34d0293b03ec3fa4983fe241efeb71c6adebe75
Seth Henderson <32090502+MCT32@users.noreply.github.com>
2025-05-24 22:49:49 +1000

    Wallpaper

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,6 +1,8 @@
+echo -e "\033[1;33mWARNING: MUST INSTALL DRACULA WALLPAPER MANUALLY"
 ln -sf $PWD/hypr $HOME/.config
 ln -sf $PWD/waybar $HOME/.config
 ln -sf $PWD/kitty $HOME/.config
 ln -sf $PWD/fish $HOME/.config
+ln -sf $PWD/qutebrowser $HOME/.config
 ln -sf $PWD/.gitconfig $HOME
 ln -sf $PWD/.vimrc $HOME
